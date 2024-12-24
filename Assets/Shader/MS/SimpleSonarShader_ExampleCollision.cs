5fa08dfe0c00223d51103ea423951c16699d0791
DESKTOP-9T83A1V\admin <kns7208@naver.com>
2024-12-24 12:53:56 +0900

    셰이더 파일들 shader/MS에 추가함

---
+++ Assets/Shader/MS/SimpleSonarShader_ExampleCollision.cs
@@ -0,0 +1,35 @@
+﻿// SimpleSonarShader scripts and shaders were written by Drew Okenfuss.
+
+using System.Collections;
+using UnityEngine;
+
+
+public class SimpleSonarShader_ExampleCollision : MonoBehaviour
+{
+    SimpleSonarShader_Parent par = null;
+
+    private void Start()
+    {
+        par = GetComponentInParent<SimpleSonarShader_Parent>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            Ray mousePosToRay = Camera.main.ScreenPointToRay(mousePos);
+
+            RaycastHit hit;
+            if (Physics.Raycast(mousePosToRay, out hit))
+            {
+                par.StartSonarRing(hit.point, 1.4f);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Start sonar ring from the contact point
+        if (par) par.StartSonarRing(collision.contacts[0].point, collision.impulse.magnitude / 10.0f);
+    }
+}
